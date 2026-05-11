82bf37f4ef0da4601fa1b5390c16eb9238942bc5
aisyahalfina <aisyahalfina04@gmail.com>
2026-05-11 10:19:11 +0700

    initial commit: chatbot groq

---
+++ src/types/Message.ts
@@ -0,0 +1,9 @@
+export interface Message {
+    role: "user" | "model";
+    content: string;
+}
+export interface ChatConfig {
+    systemInstruction: string;
+    botName: string;
+    welcomeMessage: string;
+}
